bf82cc9f4994a795de795b67cf6514b0c89baca6
trung1122-gif <71000936+trung1122-gif@users.noreply.github.com>
2020-09-30 13:29:28 +0700

    add file

--- 90/app/src/main/java/com/example/tedapp/MainActivity.java
+++ 90/app/src/main/java/com/example/tedapp/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.trungtest);
+        button = (Button) findViewById(R.id.imageButton1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, NewActivity.class);
+        startActivity(intent);
     }
 }
