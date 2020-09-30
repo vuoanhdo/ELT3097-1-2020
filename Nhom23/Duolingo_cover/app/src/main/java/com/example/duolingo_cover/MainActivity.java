71dccaffcae7fc4e5c685fe0eebc221d4b3b9385
huyanngo <71000333+huyanngo@users.noreply.github.com>
2020-09-30 21:07:47 +0700

    function

--- Nhom23/Duolingo_cover/app/src/main/java/com/example/duolingo_cover/MainActivity.java
+++ Nhom23/Duolingo_cover/app/src/main/java/com/example/duolingo_cover/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.duolingo_cover;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,22 +26,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String str = txtw.getText().toString();
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                if(str.equals("hot")){
-                    builder.setMessage("Congratulation! That's a correct answer");
-                }else{
-                    builder.setMessage("Wrong answer :((");
-                }
-
-                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-                        message.setVisibility(View.VISIBLE);
+                if(str.isEmpty()==false){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    if(str.equals("hot")){
+                        builder.setMessage("Congratulation! That's a correct answer");
+                    }else{
+                        builder.setMessage("Wrong answer :((");
                     }
-                });
-                builder.show();
-
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int i) {
+                            startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                        }
+                    });
+                    builder.show();
                 }
+            }
         });
     }
 }
