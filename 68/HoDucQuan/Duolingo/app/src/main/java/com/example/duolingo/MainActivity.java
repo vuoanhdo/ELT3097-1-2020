b57e1e7f5e5e467af296a39868ad1e38417122d0
QuanDucHo <rayoptimusprime@gmail.com>
2020-10-01 10:31:00 +0700

    update

--- 68/HoDucQuan/Duolingo/app/src/main/java/com/example/duolingo/MainActivity.java
+++ 68/HoDucQuan/Duolingo/app/src/main/java/com/example/duolingo/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.duolingo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+public void quandeptrai(View view){
+     Intent i = new Intent(this,Main2.class);
+        startActivity(i);
     }
 }
