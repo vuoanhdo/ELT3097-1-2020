d67bbf7b847af6c6d5468dd62e70468508af77d4
long0204 <longpdh02@gmail.com>
2020-10-28 11:23:37 +0700

    upload

---
+++ 01/app/src/main/java/com/example/alarmyapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.alarmyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+   Button btn
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
